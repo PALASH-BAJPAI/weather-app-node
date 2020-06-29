1164f3c2edfaebf19a22f4a21f0cdb0832e2b91f
palash <palashbajpai45@gmail.com>
2020-06-29 20:20:11 +0530

    inital commit

---
+++ public/js/app.js
@@ -0,0 +1,23 @@
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const message1 = document.querySelector('#message-1')
+const message2 = document.querySelector('#message-2')
+
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const location = search.value
+    message1.textContent = " "
+    message2.textContent = " "
+    fetch("http://localhost:3000/weather?address=" + location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                message1.textContent = data.error
+                message2.textContent = 'Try another search'
+            } else {
+                message1.textContent = data.location
+                message2.textContent = data.forecast
+            }
+        })
+    })
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,19 @@
+const request = require('request');
+const forecast = (lat, long, callback) => {
+    const url = "http://api.weatherstack.com/current?access_key=21b96f1b715bb5f195871179f3deb390&query=" + lat + "," + long + "&units=f";
+    request({ url: url, json: true }, (error, response) => {
+        if (error) {
+            callback("Unable to connect to location services", undefined);
+        } else if (response.body.location.name === null) {
+            callback('Unable to find location', undefined);
+        } else {
+            callback(undefined,
+                "weather is " + response.body.current.weather_descriptions + ".Temperature here is " + response.body.current.temperature,
+            )
+        }
+
+    })
+
+}
+
+module.exports = forecast;
