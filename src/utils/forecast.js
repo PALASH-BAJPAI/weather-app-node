d505dc582133365500225a991287cad209f6094c
palash <palashbajpai45@gmail.com>
2020-06-30 07:10:13 +0530

    units change

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,6 +1,6 @@
 const request = require('request');
 const forecast = (lat, long, callback) => {
-    const url = "http://api.weatherstack.com/current?access_key=21b96f1b715bb5f195871179f3deb390&query=" + lat + "," + long + "&units=f";
+    const url = "http://api.weatherstack.com/current?access_key=21b96f1b715bb5f195871179f3deb390&query=" + lat + "," + long + "&units=m";
     request({ url: url, json: true }, (error, response) => {
         if (error) {
             callback("Unable to connect to location services", undefined);
@@ -8,7 +8,7 @@ const forecast = (lat, long, callback) => {
             callback('Unable to find location', undefined);
         } else {
             callback(undefined,
-                "weather is " + response.body.current.weather_descriptions + ".Temperature here is " + response.body.current.temperature,
+                "weather is " + response.body.current.weather_descriptions + ".Temperature here is " + response.body.current.temperature + " C"
             )
         }
 
